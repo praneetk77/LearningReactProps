c6aaeb9dcf9cfc883396cc8a7f34dddf9832d767
Praneet Karna <praneetkarna77@gmail.com>
2021-08-23 11:51:51 +0545

    Import contacts from contacts.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Praneet"
-        number="123456"
-        email="praneet@cool.very"
-        imgUrl="https://picsum.photos/200"
+        name={contacts[0].name}
+        number={contacts[0].phone}
+        email={contacts[0].email}
+        imgUrl={contacts[0].imgURL}
       />
       <Card
-        name="Marshall"
-        number="12323123456"
-        email="eminem@notcool.very"
-        imgUrl="https://picsum.photos/200"
+        name={contacts[1].name}
+        number={contacts[1].phone}
+        email={contacts[1].email}
+        imgUrl={contacts[1].imgURL}
       />
       <Card
-        name="O'Shea"
-        number="173 7083"
-        email="ice@cube"
-        imgUrl="https://picsum.photos/200"
+        name={contacts[2].name}
+        number={contacts[2].phone}
+        email={contacts[2].email}
+        imgUrl={contacts[2].imgURL}
       />
     </div>
   );
